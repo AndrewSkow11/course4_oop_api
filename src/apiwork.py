6cff02edb1cd020d8431a600dd69f71c7c3b5d99
Andrew Skow <skovorodnikov11@gmail.com>
2023-12-05 07:21:23 +0300

    HH work example

--- src/apiwork.py
+++ src/apiwork.py
@@ -1,9 +1,47 @@
+# https://vc.ru/u/1510590-python-idea/688002-parser-hh-veb-prilozhenie-na-python
+
+
+import json
+from abc import abstractmethod
+import time
+from datetime import datetime
+from vacancy import Vacancy
+from abc import ABC, abstractmethod
+import requests
+
+
 # 1
 # Создать абстрактный класс для работы с API сайтов с вакансиями.
 # Реализовать классы, наследующиеся от абстрактного класса,
 # для работы с конкретными платформами.
 # Классы должны уметь подключаться к API и получать вакансии.
 
-class ApiWork:
+
+# Платформы для сбора вакансий
+#  hh.ru (ссылка на API: https://github.com/hhru/api)
+#  superjob.ru (ссылка на API: https://api.superjob.ru/)
+
+# Прежде чем начать использовать API от SuperJob, необходимо зарегистрироваться
+# (https://www.superjob.ru/auth/login/)
+# и получить токен для работы.
+# Подробная инструкция дается по ссылке описания документации
+# в разделе Getting started: https://api.superjob.ru/#gettin.
+# При регистрации приложения можно указать произвольные данные.
+
+class ApiWork(ABC):
+    def __init__(self):
+        print("Connecting to API ...")
+
+    @abstractmethod
+    def get_vacancies(self):
+        pass
+
+
+class HeadHunterAPI(ApiWork):
     pass
+
+class SuperJobAPI():
+    pass
+
+
 
