f6f6d826cdb96b984514940eff004ab1afa4c3f2
Andrew Skow <skovorodnikov11@gmail.com>
2023-12-08 02:48:35 +0300

    json - почти ок

--- source/api_working.py
+++ source/api_working.py
@@ -6,39 +6,52 @@ import requests
 class ConnectAPI(ABC):
     pass
 
+
 class HeadHunterAPI(ConnectAPI):
-    def __init__(self, keyword):
-        print("\033[33mConnecting to API ...\033[0m")
-        # ключевое слово или фраза для поиска вакансий
-        self.keyword = keyword
-        self.params = {
-            "text": keyword,
-            "page": 0,
-            "per_page": 100,
-        }
+    def __init__(self):
+        print("\033[33mConnecting to API ... hh.ru\033[0m")
         self.vacancies = []
+
 
     @staticmethod
     def get_salary(salary):
-        # формат от-до
+        """Метод возвращает зарплату списком ['от', 'до']"""
         formatted_salary = [None, None]
+
         if salary and salary['from'] and salary['from'] != 0:
             formatted_salary[0] = salary['from'] if salary['currency'].lower() == 'rur' else salary['from'] * 76
         if salary and salary['to'] and salary['to'] != 0:
             formatted_salary[1] = salary['to'] if salary['currency'].lower() == 'rur' else salary['to'] * 76
         return formatted_salary
 
-    """
-    Получение значений через API
-    """
-
-    def get_request(self):
+    def get_vacancies(self, keyword):
+        params = {
+            "text": keyword,
+            "page": 0,
+            "per_page": 100,
+        }
         response = requests.get("https://api.hh.ru/vacancies",
-                                # headers=self.__header,
-                                params=self.params)
+                                params=params)
         if response.status_code != 200:
             print("Не удалось совершить запрос")
-        return response.json()['items']
+
+        print("Тип данных", type(response.json()['items']))
+
+        formatted_vacancies = []
+        for vacancy in response.json()['items']:
+            salary_from, salary_to = self.get_salary(vacancy['salary'])
+            formatted_vacancies.append({
+                "id": vacancy["id"],
+                "title": vacancy["name"],
+                "url": vacancy["alternate_url"],
+                "salary_from": salary_from,
+                "salary_to": salary_to,
+                "employer": vacancy['employer']["name"],
+                "api": "HeadHunter",
+            })
+        return formatted_vacancies
+
+        # return response.json()['items']
 
     def get_formatted_vacancies(self):
         formatted_vacancies = []
@@ -55,29 +68,24 @@ class HeadHunterAPI(ConnectAPI):
             })
         return formatted_vacancies
 
-    def get_vacancies(self, pages_count=1):
-        while self.params['page'] < pages_count:
-            print(f"HeadHunter, Парсинг страницы {self.params['page'] + 1}", end=": ")
-            try:
-                values = self.get_request()
-            except Exception:
-                print("Ошибка при получении данных")
-                break
-            print(f"Найдено ({len(values)}) вакансий.")
-            self.vacancies.extend(values)
-            self.params['page'] += 1
+    # def get_vacancies(self, pages_count=1):
+    #     while self.params['page'] < pages_count:
+    #         print(f"HeadHunter, Парсинг страницы {self.params['page'] + 1}", end=": ")
+    #         try:
+    #             values = self.get_request()
+    #         except Exception:
+    #             print("Ошибка при получении данных")
+    #             break
+    #         print(f"Найдено ({len(values)}) вакансий.")
+    #         self.vacancies.extend(values)
+    #         self.params['page'] += 1
 
 
 class SuperJobAPI(ConnectAPI, ABC):
-    def __init__(self, keyword):
-        self.keyword = keyword
-        self.__header = {"X-Api-App-Id": getenv("API_KEY_SJ")}
-        self.__params = {
-            "keyword": keyword,
-            "page": 0,
-            "count": 100,
-        }
-        self.__vacancies = []
+    def __init__(self):
+        print("\033[33mConnecting to API ... superjob.ru\033[0m")
+        self.header = {"X-Api-App-Id": getenv("API_KEY_SJ")}
+        self.vacancies = []
 
     @staticmethod
     def get_salary(salary, currency):
@@ -86,18 +94,23 @@ class SuperJobAPI(ConnectAPI, ABC):
             formatted_salary = salary if currency == 'rub' else salary['from'] * 76
         return formatted_salary
 
-    def get_request(self):
+    def get_vacancies(self, keyword):
+        params = {
+            "keyword": keyword,
+            "page": 0,
+            "count": 100,
+        }
+
         response = requests.get("https://api.superjob.ru/2.0/vacancies/",
-                                headers=self.__header,
-                                params=self.__params)
-        print(response)
+                                headers=self.header,
+                                params=params)
         if response.status_code != 200:
             print("Ошибка при обращении к сайту superjob.ru")
         return response.json()['objects']
 
     def get_formatted_vacancies(self):
         formatted_vacancies = []
-        for vacancy in self.__vacancies:
+        for vacancy in self.vacancies:
             formatted_vacancies.append({
                 "id": vacancy["id"],
                 "title": vacancy["profession"],
@@ -109,23 +122,18 @@ class SuperJobAPI(ConnectAPI, ABC):
             })
         return formatted_vacancies
 
-    def get_vacancies(self, pages_count=1):
-        while self.__params['page'] < pages_count:
-            print(f"SuperJob, Парсинг страницы {self.__params['page'] + 1}", end=": ")
-            try:
-                values = self.get_request()
-            except Exception:
-                print("Ошибка при получении данных c сайта superjob.ru")
-                break
-            print(f"Найдено ({len(values)}) вакансий.")
-            self.__vacancies.extend(values)
-            self.__params['page'] += 1
+    # def get_vacancies(self, pages_count=1):
+    #     while self.params['page'] < pages_count:
+    #         print(f"SuperJob, Парсинг страницы {self.params['page'] + 1}", end=": ")
+    #         try:
+    #             values = self.get_request()
+    #         except Exception:
+    #             print("Ошибка при получении данных c сайта superjob.ru")
+    #             break
+    #         print(f"Найдено ({len(values)}) вакансий.")
+    #         self.vacancies.extend(values)
+    #         self.params['page'] += 1
+    #
 
-
-hh = HeadHunterAPI("python")
-hh.get_request()
-hh.get_vacancies()
-print(hh.get_formatted_vacancies())
-
-sj = SuperJobAPI("python")
-sj.get_vacancies()
+# sj = SuperJobAPI("python")
+# # sj.get_vacancies()
--- source/json_working.py
+++ source/json_working.py
@@ -1,35 +1,64 @@
-class Connector:
-    '''Создание класса для работы с json файлом с вакансиями'''
-    def __init__(self, keyword, vacancies_json):
-        self.__filename = f'{keyword.title()}.json'
-        self.insert(vacancies_json)
+import json
+from select import select
 
-    def insert(self, vacancies_json):
-        ''' Внесение данных о вакансиях в json файл '''
-        with open (self.__filename, 'w', encoding = 'utf-8') as file:
+from source.vacancy import Vacancy
+
+
+class JSONSaver:
+    """Создание класса для работы с json файлом с вакансиями"""
+
+    def __init__(self):
+        print("Working with json ...")
+        self.filename = 'VACANCY.json'
+
+    def add_vacancies(self, vacancies_json):
+        """Внесение данных о вакансиях в json файл"""
+
+        with open(self.filename, 'w', encoding='utf-8') as file:
             json.dump(vacancies_json, file, ensure_ascii=False, indent=4)
 
     def select(self):
-        ''' Извлечение из json файла данных о вакансии с заданными параметрами '''
-        with open(self.__filename, 'r', encoding='utf-8') as file:
+        """Извлечение из json файла данных о вакансии с заданными параметрами"""
+        with open(self.filename, 'r', encoding='utf-8') as file:
             data = json.load(file)
-        vacancies = [Vacancy(x['id'], x['title'], x['url'], x['salary_from'], x['salary_to'], x['employer'], x['api']) for x in data]
+
+
+        vacancies_from_json = []
+
+
+        vacancies = [Vacancy(x['id'],
+                             x['title'],
+                             x['url'],
+                             x['salary_from'],
+                             x['salary_to'],
+                             x['employer'],
+                             x['api']) for x in data]
         return vacancies
 
     def sorted_vacancies_by_salary_from_asc(self):
-        ''' Сортировка вакансий по минимальным зарплатам по возрастающей'''
+        """Сортировка вакансий по минимальным зарплатам по возрастающей"""
         vacancies = self.select()
         vacancies = sorted(vacancies, key=lambda p: p.salary_from or 0)
         return vacancies
 
     def sorted_vacancies_by_salary_from_desc(self):
-        ''' Сортировка вакансий по минимальным зарплатам по убывающей'''
+        """Сортировка вакансий по минимальным зарплатам по убывающей"""
         vacancies = self.select()
         vacancies = sorted(vacancies, reverse=True, key=lambda p: p.salary_from or 0)
         return vacancies
 
     def sorted_vacancies_by_salary_to_asc(self):
-        ''' Сортировка по максимальным заррплатам'''
+        """Сортировка по максимальным заррплатам"""
         vacancies = self.select()
         vacancies = sorted(vacancies, key=lambda x: x.salary_to if x.salary_to else 0)
         return vacancies
+
+    def get_vacancies_by_salary(self, num1, num2):
+        vacancies = self.select()
+        vacancies_by_salary = []
+
+        for vac in vacancies:
+            if vac.salary_from == num1 and vac.salary_to == num2:
+                vacancies_by_salary.append(vac)
+
+        return vacancies_by_salary
