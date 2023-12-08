409cad7aba160b430b05b7d902935dbfb0fd4dbc
Andrew Skow <skovorodnikov11@gmail.com>
2023-12-08 07:44:53 +0300

    почти готово

--- main_file.py
+++ main_file.py
@@ -1,6 +1,7 @@
 from source.api_working import HeadHunterAPI, SuperJobAPI
 from source.vacancy import Vacancy
 from source.json_working import JSONSaver
+import json
 
 # Создание экземпляра класса для работы с API сайтов с вакансиями
 hh_api = HeadHunterAPI()
@@ -8,7 +9,7 @@ superjob_api = SuperJobAPI()
 
 # # Получение вакансий с разных платформ
 hh_vacancies = hh_api.get_vacancies("Python")
-superjob_vacancies = superjob_api.get_vacancies("Python")
+# superjob_vacancies = superjob_api.get_vacancies("Python")
 
 # only for tests
 # vacancy = Vacancy("1234",
@@ -23,23 +24,32 @@ superjob_vacancies = superjob_api.get_vacancies("Python")
 
 # Сохранение информации о вакансиях в файл
 json_saver = JSONSaver()
-json_saver.add_vacancies(superjob_vacancies)
-# json_saver.get_vacancies_by_salary(100_000, 150_000)
-# json_saver.delete_vacancy(vacancy)
+json_saver.add_vacancies(hh_vacancies)
+json_saver.get_vacancies_by_salary(8000, 25000)
+# очистка списка
+# json_saver.clear_vacancies()
 
+
+# def filter_vacancies(js_object: JSONSaver, filter_words: list):
+#     pass
+#
+#
 def sort_vacancies(vacancies):
-    pass
+    vacancies = sorted(vacancies, reverse=True, key=lambda p: p.salary_from or 0)
 
+    # for v in vacancies:
+    #     print(v)
 
-def get_top_vacancies(sorted_vacancies, top_n):
-    pass
-
-
-def print_vacancies(vacancies):
-    pass
-
-def filter_vacancies(hh_vacancies, superjob_vacancies, filter_words):
-    pass
+    return vacancies
+#
+#
+# def get_top_vacancies(sorted_vacancies, top_n):
+#     pass
+#
+#
+# def print_vacancies(vacancies):
+#     pass
+#
 
 
 # Функция для взаимодействия с пользователем
@@ -47,16 +57,19 @@ def user_interaction():
     platforms = ["HeadHunter", "SuperJob"]
     search_query = input("Введите поисковый запрос: ")
     top_n = int(input("Введите количество вакансий для вывода в топ N: "))
+    hh_object = hh_api.get_vacancies(search_query)
+    sj_object = superjob_api.get_vacancies(search_query)
+
     filter_words = input("Введите ключевые слова для фильтрации вакансий: ").split()
-    filtered_vacancies = filter_vacancies(hh_vacancies, superjob_vacancies, filter_words)
+    filtered_vacancies = json_saver.filter_vacancies(filter_words)
 
     if not filtered_vacancies:
         print("Нет вакансий, соответствующих заданным критериям.")
         return
 
     sorted_vacancies = sort_vacancies(filtered_vacancies)
-    top_vacancies = get_top_vacancies(sorted_vacancies, top_n)
-    print_vacancies(top_vacancies)
+    # top_vacancies = get_top_vacancies(sorted_vacancies, top_n)
+    # print_vacancies(top_vacancies)
 
 
 if __name__ == "__main__":
--- source/json_working.py
+++ source/json_working.py
@@ -31,11 +31,11 @@ class JSONSaver:
                              x['api']) for x in data]
         return vacancies
 
-    def sorted_vacancies_by_salary_from_asc(self):
-        """Сортировка вакансий по минимальным зарплатам по возрастающей"""
-        vacancies = self.select()
-        vacancies = sorted(vacancies, key=lambda p: p.salary_from or 0)
-        return vacancies
+    # def sorted_vacancies_by_salary_from_asc(self):
+    #     """Сортировка вакансий по минимальным зарплатам по возрастающей"""
+    #     vacancies = self.select()
+    #     vacancies = sorted(vacancies, key=lambda p: p.salary_from or 0)
+    #     return vacancies
 
     def sorted_vacancies_by_salary_from_desc(self):
         """Сортировка вакансий по минимальным зарплатам по убывающей"""
@@ -43,11 +43,11 @@ class JSONSaver:
         vacancies = sorted(vacancies, reverse=True, key=lambda p: p.salary_from or 0)
         return vacancies
 
-    def sorted_vacancies_by_salary_to_asc(self):
-        """Сортировка по максимальным заррплатам"""
-        vacancies = self.select()
-        vacancies = sorted(vacancies, key=lambda x: x.salary_to if x.salary_to else 0)
-        return vacancies
+    # def sorted_vacancies_by_salary_to_asc(self):
+    #     """Сортировка по максимальным заррплатам"""
+    #     vacancies = self.select()
+    #     vacancies = sorted(vacancies, key=lambda x: x.salary_to if x.salary_to else 0)
+    #     return vacancies
 
     def get_vacancies_by_salary(self, num1, num2):
         vacancies = self.select()
@@ -58,3 +58,30 @@ class JSONSaver:
                 vacancies_by_salary.append(vac)
 
         return vacancies_by_salary
+
+    def clear_vacancies(self):
+        with open(self.filename, 'w', encoding='utf-8') as file:
+            file.write("")
+
+    def filter_vacancies(self, list_of_word):
+        vacancies = self.select()
+        vacancies_filter = []
+
+        for word in list_of_word:
+            for vacancy in vacancies:
+                if word in vacancy.title:
+                    vacancies_filter.append(vacancy)
+
+        return vacancies_filter
+
+    # def sort_vacancies(self, vacancies):
+    #
+    #
+    # def get_top_vacancies(self, sorted_vacancies, top_n):
+    #     pass
+    #
+    # def print_vacancies(self, vacancies):
+    #     pass
+    #
+
+
